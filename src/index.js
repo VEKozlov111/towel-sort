48965df5f10940a22fa2860cd9a378a69e62b8f3
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2022-08-10 19:42:29 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+
+  let a = matrix.map(function (item, index, array) {
+    item.join();
+    return array.join(',')
+  });
+  return a[0].split(',').map(function (item) { return Number(item) })
+
+};
+/*console.log(towelSort([
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+]));*/
